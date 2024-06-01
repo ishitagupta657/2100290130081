419535b38009e27d6347a0a095d8ffeac13653b0
Ishita gupta <ishitagupta@Ishitas-MacBook-Air.local>
2024-06-01 11:42:04 +0530

    fixrouting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/products',
+          permanent: false, // Use true if this is a permanent redirect
+        },
+      ]
+    },
+  }
+  
+  export default nextConfig
